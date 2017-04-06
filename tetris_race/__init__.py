89ac98ee9232ed63ee6aff10587be8ca8d86038b
onemorex <gushchin.iv@gmail.com>
2017-04-06 20:16:02 +0300

    Change init.py

--- tetris_race/__init__.py
+++ tetris_race/__init__.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 register(
     id='TetrisRace-v0',
-    entry_point='gym_tetris_race.envs:TetrisRaceEnv',
+    entry_point='tetris_race.envs:TetrisRaceEnv',
     timestep_limit=1000,
     reward_threshold=1.0,
     nondeterministic = True,
